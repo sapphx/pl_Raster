2a09e0be1e18e0d1cbf6759a3823fc02a375bb88
witold.rzasa <witold.rzasa@unit9.com>
2015-12-14 21:47:48 +0100

    - initial commit

---
+++ Renderer/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Renderer
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose ( bool disposing )
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent ()
+		{
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(586, 437);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(384, 416);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 437);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
+
---
+++ Renderer/float4x4.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renderer
+{
+	class float4x4
+	{
+
+		private float4[] vals;
+
+		#region Constructors
+
+		public  float4x4 ()
+		{
+			vals = new float4[4] { new float4(), new float4 (), new float4 (), new float4 () };
+		}
+
+		public float4x4 ( float4 x, float4 y, float4 z, float4 w )
+		{
+			vals = new float4[4] { x, y, z, w };
+		}
+
+		#endregion
+
+		#region Operators
+
+		public float4 this[int i]
+		{
+			get { return vals[i]; }
+			set { vals[i] = value; }
+		}
+
+		public	override	string	ToString ()
+		{
+			return vals[0].ToString() + '\n' + vals[1].ToString() + '\n' + vals[2].ToString() + '\n' + vals[3].ToString();
+		}
+
+		public	string	ToString ( string split)
+		{
+			return vals[0].ToString() + split + vals[1].ToString() + split + vals[2].ToString() + split + vals[3].ToString();
+		}
+
+		//float4
+
+		public static float4x4 operator - ( float4x4 a, float4x4 b )
+		{
+			return new float4x4(a[0] - b[0], a[1] - b[1], a[2] - b[2], a[3] - b[3]);
+		}
+
+		public static float4x4 operator + ( float4x4 a, float4x4 b )
+		{
+			return new float4x4(a[0] + b[0], a[1] + b[1], a[2] + b[2], a[3] + b[3]);
+		}
+
+		public static float4x4 operator * ( float4x4 a, float4x4 b )
+		{
+			return new float4x4(a[0] * b[0], a[1] * b[1], a[2] * b[2], a[3] * b[3]);
+		}
+
+		public static float4x4 operator / ( float4x4 a, float4x4 b )
+		{
+			return new float4x4(a[0] / b[0], a[1] / b[1], a[2] / b[2], a[3] / b[3]);
+		}
+
+		//floats
+
+		public static float4x4 operator * ( float4x4 a, float b )
+		{
+			return new float4x4(a[0] * b, a[1] * b, a[2] * b, a[3] * b);
+		}
+
+		public static float4x4 operator / ( float4x4 a, float b )
+		{
+			return new float4x4(a[0] / b, a[1] / b, a[2] / b, a[3] / b);
+		}
+
+		#endregion
+
+		#region Functions
+
+		public float4x4 Transpose ()
+		{
+			return  new float4x4(	new	float4(vals[0][0],	vals[1][0], vals[2][0], vals[3][0]),
+									new	float4(vals[0][1],	vals[1][1], vals[2][1], vals[3][1]),
+									new	float4(vals[0][2],	vals[1][2], vals[2][2], vals[3][2]),
+									new	float4(vals[0][3],	vals[1][3], vals[2][3], vals[3][3]));
+		}
+
+		#endregion
+
+	}
+}
