0af4a480721ac086f258a9dc366a4278bfb9d8c4
witold.rzasa <witold.rzasa@unit9.com>
2016-01-09 20:12:29 +0100

    - added mesh and simple shapes (triangle and cube) the rest is TODO

--- Renderer/Form1.Designer.cs
+++ Renderer/Form1.Designer.cs
@@ -40,18 +40,21 @@
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(586, 437);
+            this.panel1.Size = new System.Drawing.Size(586, 277);
             this.panel1.TabIndex = 0;
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.pictureBox1.Location = new System.Drawing.Point(10, 11);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(384, 416);
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -59,9 +62,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(586, 437);
+            this.ClientSize = new System.Drawing.Size(586, 277);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
---
+++ Renderer/Vertex.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renderer
+{
+    class Vertex
+    {
+        public	float3  Position;
+        public	float3  Normal;
+
+		#region Constructors
+
+		public	Vertex()
+		{
+			this.Position	= new float3();
+			this.Normal		= new float3();
+		}
+
+		public	Vertex(float3 positionOnly)
+		{
+			this.Position = positionOnly;
+			this.Normal = new float3();
+		}
+
+		public Vertex(float3 position, float3 normal)
+		{
+			this.Position = position;
+			this.Normal = normal;
+		}
+
+		#endregion
+
+    }
+}
