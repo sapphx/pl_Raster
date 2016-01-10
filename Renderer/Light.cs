44f66c4193fd43732f964e0d92e9f0be68718919
Witold Rząsa <sapphx@gmail.com>
2016-01-10 02:05:18 +0100

    - work on Light begun

---
+++ Renderer/Light.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renderer
+{
+	class Light
+	{
+		public	float3	Position; 
+		public	float3	Ambient;
+		public	float3	Diffuse;
+		public	float3	Specular;
+		public	float	Shininess;
+
+		#region Constructors
+
+		public  Light() {}
+
+		public	Light(float3 position, float3 ambient, float3 diffuse, float3 specular, float shininess) 
+		{
+			this.Position = position;
+			this.Ambient = ambient;
+			this.Diffuse = diffuse;
+			this.Specular = specular;
+			this.Shininess = shininess;
+		}
+
+		#endregion
+
+		#region Functions
+
+		public	virtual	float3	Calculate(Fragment fragment, VertexProcessor vertexProcessor)
+		{
+			return new float3();
+		}
+
+		#endregion
+	}
+}
---
+++ Renderer/Lights/DirectionalLight.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renderer.Lights
+{
+	class DirectionalLight : Light
+	{
+		
+		#region Constructors
+
+			public  DirectionalLight() : base() { }
+
+			public DirectionalLight(float3 position, float3 ambient, float3 diffuse, float3 specular, float shininess) : base(position, ambient, diffuse, specular, shininess) { }
+
+		#endregion
+
+		public	override	float3	Calculate(Fragment fragment, VertexProcessor vertexProcessor)
+		{
+			//TODO
+		}
+	}
+}
